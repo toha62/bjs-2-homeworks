deb97f8c8ec30e0cca12c03a3315d8b7b2735583
Anton Trifonov <toha62@inbox.ru>
2022-10-08 14:30:30 +0300

    task arrays complete

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,15 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  if (arr1.length === arr2.length) {
+    return arr1.every((item, index) => item === arr2[index]);
+  } else {
+    return false;
+  }   
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter(item => item > 0 && !(item % 3)).map(item => item * 10);
 
   return resultArr; // array
 }
