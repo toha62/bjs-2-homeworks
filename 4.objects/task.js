f994156239ebb03e9cf1a5a5cf14a413b50ddbc8
Anton Trifonov <toha62@inbox.ru>
2022-10-12 11:57:54 +0300

    objects task complete

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,49 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age; 
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) { 
+    this.marks = []; 
+  } 
+
+  this.marks.push(mark);    
+}
+
+Student.prototype.addMarks = function (...marksArray) {
+  if (this.marks === undefined) { 
+    this.marks = []; 
+  } 
+
+  this.marks.push(...marksArray);    
+}
+
+Student.prototype.getAverage = function () {
+  if (this.marks === undefined || this.marks === []) { 
+    console.log(`${this.name} ещё не имеет оценок`); 
+    return;
+  } 
+
+  let average = this.marks.reduce((sum, item) => sum + item, 0) / this.marks.length;
+  // average = Number(average.toFixed(1));
+
+  return(average);    
+}
+
+Student.prototype.exclude = function (reason) {
+  this.excluded = reason;
+  
+  if (this.hasOwnProperty('subject')) {
+    delete this.subject;    
+  }
+
+  if (this.hasOwnProperty('marks')) {
+    delete this.marks;    
+  }
+}
