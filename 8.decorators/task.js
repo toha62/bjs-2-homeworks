f76e9844d45d948c2e19bb758a01ce83b7237af6
Anton Trifonov <toha62@inbox.ru>
2022-11-03 16:38:42 +0300

    task Async complete

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,50 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  const cache = {};
+  const cacheLength = 5;
+
+  return (...args) => {
+    // Создадим хэш путём склеивания аргументов в строку с префиксом "_"
+    // Префикс нужен для того, чтобы хэш нельзя было преобразовать в число (даже когда аргумент один).
+    // Тогда можно с помощью Object.keys() перечислять св-ва обьекта cache в том порядке в котором они
+    // добавлялись к обьекту.
+    let hash = args.reduce((acc, item) => acc + item, '_');    
+
+    if (hash in cache) {
+      console.log(`Из кэша: ${cache[hash]}`);        
+      return `Из кэша: ${cache[hash]}`;         
+    } 
+
+    console.log(`Вычисляем: ${cache[hash] = func(...args)}`);
+    if (Object.keys(cache).length > cacheLength) {
+      delete cache[Object.keys(cache)[0]];
+    }  
+        
+    return `Вычисляем: ${cache[hash]}`; 
+  }
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, delay) {
+  let timeoutId = null;
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+
+  function wrapper(... args) {
+    if (!wrapper.allCount++) {
+      wrapper.count++;      
+      func(...args);
+    }
+
+    if (timeoutId) {      
+      clearTimeout(timeoutId);
+    }
+    
+    timeoutId = setTimeout(() => {
+      wrapper.count++;
+      timeoutId = null;
+      func(...args);      
+    }, delay);
+  }
+  
+  return wrapper;
 }
